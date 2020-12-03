54e53fde6b8c8105d21869800b7ac10b2923e685
Phan_Chinh_Quang <pchinhquang@gmail.com>
2020-12-03 22:24:07 +0700

    doi

---
+++ BaiTap4/src/main/java/bai1/Bai1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai1;
+
+/**
+ *
+ * @author User
+ */
+public class Bai1 {
+    public static void main(String[] args) {
+        try{
+            new KhachHang("tdhgd", "lien khe - khoai chau", "");
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ BaiTap4/src/main/java/bai1/MatHang.java
@@ -0,0 +1,12 @@
+
+package bai1;
+
+public class MatHang {
+    static private int ma = 1000;
+    private String tenHang, nhomHang;
+    private double giaBan;
+    
+    public MatHang(){
+        ma++;
+    }
+}
